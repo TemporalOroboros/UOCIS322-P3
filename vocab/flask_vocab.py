f4c3700e1a87cb0c249e23bed5e04307fc7c9954
Ethan <TemporalOroboros@gmail.com>
2021-05-03 02:06:59 -0700

    Commit

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -6,6 +6,9 @@ from a scrambled string)
 
 import flask
 import logging
+import random
+import string
+from os import urandom
 
 # Our modules
 from src.letterbag import LetterBag
@@ -20,17 +23,16 @@ import src.config as config
 app = flask.Flask(__name__)
 
 CONFIG = config.configuration()
-app.secret_key = CONFIG.SECRET_KEY  # Should allow using session variables
+app.secret_key = urandom(24)	# Should allow using session variables
 
 #
 # One shared 'Vocab' object, read-only after initialization,
-# shared by all threads and instances.  Otherwise we would have to
+# shared by all threads and instances.	Otherwise we would have to
 # store it in the browser and transmit it on each request/response cycle,
 # or else read it from the file on each request/responce cycle,
 # neither of which would be suitable for responding keystroke by keystroke.
 
 WORDS = Vocab(CONFIG.VOCAB)
-
 
 ###
 # Pages
@@ -39,142 +41,109 @@ WORDS = Vocab(CONFIG.VOCAB)
 @app.route("/")
 @app.route("/index")
 def index():
-    """The main page of the application"""
-    flask.g.vocab = WORDS.as_list()
-    flask.session["target_count"] = min(
-        len(flask.g.vocab), CONFIG.SUCCESS_AT_COUNT)
-    flask.session["jumble"] = jumbled(
-        flask.g.vocab, flask.session["target_count"])
-    flask.session["matches"] = []
-    app.logger.debug("Session variables have been set")
-    assert flask.session["matches"] == []
-    assert flask.session["target_count"] > 0
-    app.logger.debug("At least one seems to be set correctly")
-    return flask.render_template('vocab.html')
+	"""The main page of the application"""
+	flask.g.vocab = WORDS.as_list()
+	
+	flask.session["target_count"] = min(
+		len(flask.g.vocab), CONFIG.SUCCESS_AT_COUNT)
+	flask.session["jumble"] = jumbled(
+		flask.g.vocab, flask.session["target_count"])
+	flask.session["matches"] = []
+	flask.session["success"] = False
+	app.logger.debug("Session variables have been set")
 
-
-@app.route("/keep_going")
-def keep_going():
-    """
-    After initial use of index, we keep the same scrambled
-    word and try to get more matches
-    """
-    flask.g.vocab = WORDS.as_list()
-    return flask.render_template('vocab.html')
+	assert flask.session["matches"] == [], "Matches found on initial load"
+	assert flask.session["target_count"] >= 0, "Target count is below or equal to zero"
+	assert not flask.session["success"], "Success has been registered at init"
+	app.logger.debug("At least one seems to be set correctly")
+	
+	return flask.render_template('vocab.html')
 
 
 @app.route("/success")
 def success():
-    return flask.render_template('success.html')
-
-
-#######################
-# Form handler.
-#   You'll need to change this to a
-#   a JSON request handler
-#######################
-
-@app.route("/_check", methods=["POST"])
-def check():
-    """
-    User has submitted the form with a word ('attempt')
-    that should be formed from the jumble and on the
-    vocabulary list.  We respond depending on whether
-    the word is on the vocab list (therefore correctly spelled),
-    made only from the jumble letters, and not a word they
-    already found.
-    """
-    app.logger.debug("Entering check")
-
-    # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
-    jumble = flask.session["jumble"]
-    matches = flask.session.get("matches", [])  # Default to empty list
-
-    # Is it good?
-    in_jumble = LetterBag(jumble).contains(text)
-    matched = WORDS.has(text)
-
-    # Respond appropriately
-    if matched and in_jumble and not (text in matches):
-        # Cool, they found a new word
-        matches.append(text)
-        flask.session["matches"] = matches
-    elif text in matches:
-        flask.flash("You already found {}".format(text))
-    elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
-    elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
-    else:
-        app.logger.debug("This case shouldn't happen!")
-        assert False  # Raises AssertionError
-
-    # Choose page:  Solved enough, or keep going?
-    if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
-    else:
-       return flask.redirect(flask.url_for("keep_going"))
-
+	if not flask.session["success"]:
+		flask.abort(403)
+	return flask.render_template('success.html'), 200
 
 ###############
 # AJAX request handlers
-#   These return JSON, rather than rendering pages.
+#	These return JSON, rather than rendering pages.
 ###############
 
-@app.route("/_example")
-def example():
-    """
-    Example ajax request handler
-    """
-    app.logger.debug("Got a JSON request")
-    rslt = {"key": "value"}
-    return flask.jsonify(result=rslt)
+@app.route("/_check")
+def check():
+	"""
+	Checks whether a text string a user has
+	entered is a valid word for this game.
+	"""
+	app.logger.debug("Entering check")
+
+	# The data we need, from the from and cookies.
+	text = flask.request.args.get("text", type=str)
+	jumble = flask.session["jumble"]
+	matches = flask.session.get("matches", []) # Default to an empty list
+
+	# Failure checks:
+	app.logger.debug("Checking text string: {}".format(text))
+	if text in matches:
+		return flask.jsonify(result={"stat": "failure", "message": "duplicate"}), 200
+	if not WORDS.has(text):
+		return flask.jsonify(result={"stat": "failure", "message": "no_match"}), 200
+	if not LetterBag(jumble).contains(text):
+		return flask.jsonify(result={"stat": "failure", "message": "anagram"}), 200
+	
+	# Success state:
+	matches.append(text)
+	flask.session["matches"] = matches
+	if len(matches) < flask.session["target_count"]:
+		return flask.jsonify(result={"stat": "success", "text": text}), 200
+	
+	flask.session["success"] = True
+	return flask.jsonify(result={"stat": "success", "text": text, "redirect": flask.url_for("success")}), 200
 
 
 #################
 # Functions used within the templates
 #################
 
-@app.template_filter('filt')
+@app.template_filter('to_text')
 def format_filt(something):
-    """
-    Example of a filter that can be used within
-    the Jinja2 code
-    """
-    return "Not what you asked for"
+	"""
+	Example of a filter that can be used within
+	the Jinja2 code
+	"""
+	return "{}".format(something)
 
 ###################
-#   Error handlers
+#	Error handlers
 ###################
-
 
 @app.errorhandler(404)
 def error_404(e):
-    app.logger.warning("++ 404 error: {}".format(e))
-    return flask.render_template('404.html'), 404
+	app.logger.warning("++ 404 error: {}".format(e))
+	return flask.render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def error_500(e):
-    app.logger.warning("++ 500 error: {}".format(e))
-    assert not True  # I want to invoke the debugger
-    return flask.render_template('500.html'), 500
+	app.logger.warning("++ 500 error: {}".format(e))
+	assert not True  # I want to invoke the debugger
+	return flask.render_template('500.html'), 500
 
 
 @app.errorhandler(403)
 def error_403(e):
-    app.logger.warning("++ 403 error: {}".format(e))
-    return flask.render_template('403.html'), 403
+	app.logger.warning("++ 403 error: {}".format(e))
+	return flask.render_template('403.html'), 403
 
 
 #############
 
 if __name__ == "__main__":
-    if CONFIG.DEBUG:
-        app.debug = True
-        app.logger.setLevel(logging.DEBUG)
-        app.logger.info(
-            "Opening for global access on port {}".format(CONFIG.PORT))
-        app.run(port=CONFIG.PORT, host="0.0.0.0")
+	if CONFIG.DEBUG:
+		app.debug = True
+		app.logger.setLevel(logging.DEBUG)
+		app.logger.info(
+			"Opening for global access on port {}".format(CONFIG.PORT))
+		app.run(port=CONFIG.PORT, host="0.0.0.0")
